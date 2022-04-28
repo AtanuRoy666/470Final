4cf35f2759bf3586c5c949fee195a4d0fe7571eb
Atanu Roy <atanuroy316@gmail.com>
2022-04-28 23:42:50 +0600

    layout changes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -25,17 +25,20 @@
                 </div>
             </form> -->
             <!-- Navbar-->
-            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <!-- <li><a class="dropdown-item" href="#!">Settings</a></li>
-                        <li><a class="dropdown-item" href="#!">Activity Log</a></li> -->
-                        <!-- <li><hr class="dropdown-divider" /></li> -->
-                        <li><a class="dropdown-item" href="{{route('user_logout')}}">Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
+            <div class="d-flex flex-row-reverse bd-highlight">
+            
+                <ul class="navbar-nav ">
+                    <li class="nav-item dropdown">
+                        <div class="ms-auto p-2">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="{{route('user_logout')}}">Logout</a></li>
+                            </ul>
+                        </div>
+                        
+                    </li>
+                </ul>
+            </div>
         </nav>
         <div id="layoutSidenav">
             <div id="layoutSidenav_nav">
